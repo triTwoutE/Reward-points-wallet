f48ad60e65fbb894bd88ebe70000f8510524e408
TienThanh <tienthanh31200@gmail.com>
2025-06-04 15:51:48 +0700

    User class

--- user.cpp
+++ user.cpp
@@ -1,14 +1,39 @@
-#include <bits/stdc++.h>
 #include "user.h"
-#include <fstream>
 
 using namespace std;
 
 //Constructor:
 user :: user(){
     //Set Fullname
+    setfullname();
+    //Set Email:
+    setemail();
+    //Set Username;
+    setusername();
+    //Set password
+    setpwd();
+}
+
+void user::save (){
+    string sf = "userdata.txt";
+    while (search_username(username,sf) > 0){
+        cout << "That username is taken. Try another!" << endl;
+        this->setusername();
+        this->setpwd();
+    }    
+        fstream savefile(sf,ios::out | ios::app);
+        savefile << username << "|" << passwordHash << "|" << fullname << "|" << email  
+        << "|" << walletID << endl;
+        savefile.close();
+    }
+
+void user::setfullname(){
+    //Set Fullname
     cout << "Fullname: ";
     getline(cin,fullname);
+}
+
+void user::setemail(){
     //Set Email:
     cout << "Email: ";
     getline(cin,email);
@@ -23,6 +48,9 @@ user :: user(){
         cout << "Email not valid. Press any key and try again!";
         getchar();
     } */
+}
+
+void user::setusername(){
     //Set Username;
     while(true){
         cout << "Username (just contain lowcase character and num): ";
@@ -31,7 +59,9 @@ user :: user(){
         cout << "Username not valid. Press Enter and try again!";
         getchar();
     }
-    //Set password
+}
+
+void user::setpwd(){
     string pwd;
     while(true){
         cout << "Password (8 digit not contain space): ";
@@ -43,12 +73,13 @@ user :: user(){
         }
     }
     this->passwordHash = hashstring(pwd);
-
-
 }
 
+void setwalletID(){
 
-//Function:
+};
+
+//Function
 string hashstring(string s){
     return "Hash" + s;  //Tamthoi
 }
@@ -79,12 +110,42 @@ bool checkpassword(string s){
 //     }
 // }
 
-void user::save (){
-    fstream savefile("userdata.txt",ios::out | ios::app);
-    savefile << fullname << "|" << email 
-    << "|" << username << "|" << passwordHash << endl;
-    savefile.close();
+
+
+
+void backupdata(){
+    ifstream datafile("userdata.txt",ios::binary);
+    if (!datafile) cerr << "Can't find data file to back up";
+    time_t timestamp = time(NULL);
+    time(&timestamp);
+    struct tm currenttime = *localtime(&timestamp);
+    char thoigian[50];
+    strftime(thoigian, 50, "%y_%b_%d_%I_%M_%p",&currenttime);
+    string tg = thoigian;
+    string out = "backup/" + tg + ".txt";
+    ofstream backupfile(out, ios::binary);
+    backupfile << datafile.rdbuf();
+    
+}
+
+
+int search_username(string usernamecheck,string sf){
+    fstream datafile(sf,ios::in);
+    int row = 1;
+    string line;
+    while (getline(datafile,line)){
+        stringstream ss(line);
+        string username;
+        getline(ss, username, '|');
+        if (usernamecheck == username) return row;
+        row++;
+    }
+    return 0;
 
 }
 
-void login(){}
+void login(){
+
+}
+
+
--- user.h
+++ user.h
@@ -1,24 +1,33 @@
 #ifndef user_h_
 #define user_h_h
 #include <bits/stdc++.h>
+#include <fstream> //FILE
+#include <ctime> //TIME
+#include <sstream> //string
 
 using namespace std;
-
-string hashstring (string);
-bool checkusername(string);
-bool checkpassword(string);
-bool checkemail(string);
-void createuser();
 
 class user {
     private:
         string username, passwordHash, fullname, email, walletID;
     public:
         user();
-        user(string, string, string);
-        void save ();
-        void login();
-
+        void save();
+        void setfullname();
+        void setemail();
+        void setusername();
+        void setpwd();
+        void setwalletID();
 
 };
+
+string hashstring (string);
+bool checkusername(string);
+bool checkpassword(string);
+bool checkemail(string);
+void createuser();
+void backupdata();
+int search_username(string,string);
+void login();
+
 #endif
